7e85af6fb016f3e7e4e94bd5badde7c31f865a17
Samuel Davenport <sdavenport@Samuels-MacBook-Pro.local>
2025-04-04 14:32:49 +0100

    backup

--- GradientDemo.m
+++ GradientDemo.m
@@ -1,4 +1,4 @@
-N=50;
+N=40;
 V=300;
 K=10;
 rho0=0.05;
@@ -17,6 +17,8 @@ addnoise = 1;
 if addnoise == 1
     Y = Y + randn(size(Y))*0.75;
 end
+
+% Y = randn(N,V)
 
 S = corrcoef(Y);
 [V,D]=eig(S);V=fliplr(V);D=flip(diag(D));
@@ -75,4 +77,72 @@ grads = score(:, 1:ngrads);
 plot(grads(:,1), grads(:,2), '*')
 
 %%
-Sig + rand()
+N=100;
+V=300;
+K=10;
+rho0=0.05;
+% rho0 = 0;
+rho=0.5;
+% rhoMin=0.5; % minimum non-zero fraction of rho in a block
+rhoMin=1;
+
+xx=linspace(1,rhoMin,V/K);
+Sig0=toeplitz(xx);
+Sig=rho0+(1-rho)*eye(V)+rho*kron(eye(K),Sig0);
+
+
+addnoise = 1;
+if addnoise == 1
+    Y = Y + randn(size(Y))*0.75;
+end
+
+S = corrcoef(Y);
+[V,D]=eig(S);V=fliplr(V);D=flip(diag(D));
+
+% subplot(2,2,1);imagesc(Sig,[-0.1,min(1,rho*2)]);axis image;colorbar
+subplot(2,3,1);imagesc(S,[-0.1,min(1,rho*2)]);axis image;colorbar
+grmat_top = keep_top(S, 0.1);
+subplot(2,3,2);imagesc(grmat_top,[-0.1,min(1,rho*2)]);axis image;colorbar
+grmat_grad = normalized_angle(grmat_top);
+subplot(2,3,3);imagesc(grmat_grad,[-0.1,min(1,rho*2)]);axis image;colorbar
+
+% subplot(2,2,3);plot(V(:,1:3));set(get(gca,'Children'),'LineWidth',2)
+% legend({'PC1','PC2','PC3'})
+[V,D]=eig(S); V=fliplr(V);
+subplot(2,3,4);
+[coeff, score, ~] = pca(S);
+ngrads = 2;
+grads = score(:, 1:ngrads);
+
+plot(grads(:,1), grads(:,2), '*')
+title('PCA')
+% plot(V(:,1),V(:,2), '*')
+% [coeff, score, ~] = pca(S);
+% grads = score(:, 1:ngrads);
+% 
+% plot(grads(:,1), grads(:,2), '*')
+
+subplot(2,3,5)
+
+% grmat_grad = S;
+% [V, D] = eig(grmat_grad); V=fliplr(V);
+[coeff, score, ~] = pca(grmat_top);
+grads = score(:, 1:ngrads);
+
+plot(grads(:,1), grads(:,2), '*')
+title('Just thresholding')
+
+subplot(2,3,6)
+
+% grmat_grad = S;
+% [V, D] = eig(grmat_grad); V=fliplr(V);
+[coeff, score, ~] = pca(grmat_grad);
+grads = score(:, 1:ngrads);
+
+plot(grads(:,1), grads(:,2), '*')
+title('Gradients')
+% subplot(2,2,4); plot(V(:,1),V(:,2), '*')
+% xlim
+fullscreen
+% plot(grmat_grad*V(:,1), grmat_grad*V(:,2), '*')
+BigFont(25)
