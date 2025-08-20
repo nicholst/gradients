b11f93ece294ad01d2e83fb1eb4033cf7316711f
nicholst <thomas.nichols@bdi.ox.ac.uk>
2025-08-20 16:01:38 +0100

    Add gitignore, WK_FCGreg.m

---
+++ WK_FCGreg.m
@@ -0,0 +1,16 @@
+FCGreg('HCP',[],3,0,0)
+FCGreg('HCP',[],3,2,0)
+FCGreg('HCP',[],3,4,0)
+FCGreg('HCP',[],3,0,1)
+FCGreg('HCP',[],3,2,1)
+FCGreg('HCP',[],3,4,1)
+
+FCGreg('SCZ',[],5,0,0)
+FCGreg('SCZ',[],5,2,0)
+FCGreg('SCZ',[],5,4,0)
+FCGreg('SCZ',[],5,0,1)
+FCGreg('SCZ',[],5,2,1)
+FCGreg('SCZ',[],5,4,1)
+
+
+
